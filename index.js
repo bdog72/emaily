b64bd9590090744262c92e7e0d8395f1ce4fbf9b
Brian Bycynski <high5bri@hotmail.com>
2017-08-15 06:28:38 -0400

    trying again

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send({ what: 'Up Bdog, you are a bozo' })
+  res.send(
+    { what: 'Up Bdog, you are a bozo',
+      hello: 'Dork'
+    }
+  )
 })
 
 const PORT = process.env.PORT || 5000
