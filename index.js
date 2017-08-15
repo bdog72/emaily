8e2f5083c741f0c3f1e95b3e2ea72679a413e01f
Brian Bycynski <high5bri@hotmail.com>
2017-08-15 05:46:28 -0400

    trying to deploy again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send({ what: 'Up Bdog' })
+  res.send({ what: 'Up Bdog, you are a bozo' })
 })
 
 const PORT = process.env.PORT || 5000
