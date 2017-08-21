9816d471ea1ce532dda0a10060a2e46f0009ecde
Brian Bycynski <high5bri@hotmail.com>
2017-08-21 07:50:09 -0400

    finished section 7 lexture 83

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Header extends Component {
+
+  renderContent () {
+    switch (this.props.auth) {
+      case null:
+        return
+      case false:
+        return (
+          <li><a href='/auth/google'>Login With Google</a></li>
+        )
+      default:
+        return <li><a>Log Out</a></li>
+    }
+  }
+
   render () {
     return (
       <nav>
-        <div className='nav-wrapper'>
+        <div className='nav-wrapper blue'>
           <a className='left brand-logo'>Emaily</a>
           <ul className='right'>
-            <li>
-              <a href='/auth/google'>Login With Google</a>
-            </li>
+            { this.renderContent() }
           </ul>
         </div>
       </nav>
     )
   }
 }
-export default Header
+
+function mapStateToProps ({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Header)
