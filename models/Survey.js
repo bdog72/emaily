d480fb570762acf58188e3a939781512b51cafa4
Brian Bycynski <high5bri@hotmail.com>
2017-08-25 11:39:28 -0400

    finished section 10 lecture 127

--- models/Survey.js
+++ models/Survey.js
@@ -1,12 +1,13 @@
 
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+const RecipientSchema = require('./Recipient')
 
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String],
+  recipients: [RecipientSchema],
   yes: {
     type: Number,
     default: 0
@@ -14,7 +15,13 @@ const surveySchema = new Schema({
   no: {
     type: Number,
     default: 0
-  }
+  },
+  _user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  dateSent: Date,
+  lastResponded: Date
 })
 
 mongoose.model('surveys', surveySchema)
